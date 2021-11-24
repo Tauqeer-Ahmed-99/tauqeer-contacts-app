530cf7fcd8c31e3ca3db26edf5df0ec394f09bc4
Tauqeer-Ahmed-99 <tauqueerrkhan@gmail.com>
2021-11-24 10:58:27 +0530

    react app setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <body className="box-border grid h-screen p-0 m-0 place-items-center">
+      <div>
+        <h1 >Hello Tauqeer!!!</h1>
+        <p>Hope you are doing fine.😇</p>
+        <p>Start working on your Project.😊</p>
+        <p>
+          And don't forget to install dependencies and required libraries.📚
+        </p>
+        <p>Happy Coding!🚀</p>
+      </div>
+    </body>
+  );
+};
+
+export default App;
